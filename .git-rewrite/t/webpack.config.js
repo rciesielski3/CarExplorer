365844628fc8bdac2ca418b7c89655c7c90855ee
r.ciesielski3 <r.ciesielski3@gmail.com>
2026-07-02 07:39:14 +0200

    chore: remove .git-rewrite artifacts and internal docs from repo
    - Remove accidental .git-rewrite directory (276 files)
    - Move internal docs to local only (codex.md, gemini.md, planning docs)
    - Keep only essential project files: source, tests, release notes, README
    - Update .gitignore to prevent future commits of internal/temporary files

--- .git-rewrite/t/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const createExpoWebpackConfigAsync = require("@expo/webpack-config");
-
-module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
-  config.output.scriptType = "text/javascript";
-
-  return config;
-};
