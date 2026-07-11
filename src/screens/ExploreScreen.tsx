rciesielski3/CarExplorer

commit dcf99e135963e8e48e9e048b40cd3ed16307347e

fix: add FlatList optimization props (maxToRenderPerBatch, updateCellsBatchingPeriod)

src/screens/ExploreScreen.tsx
@@ -268,11 +268,14 @@ const ExploreScreen = () => {
               <FlatList
                 data={displayedModels}
                 keyExtractor={(item) => item.id.toString()}
+                numColumns={numColumns}
                 style={styles.flexFill}
                 contentContainerStyle={[
                   styles.listContent,
                   styles.listContentWithStickyAction,
                 ]}
+                maxToRenderPerBatch={12}
+                updateCellsBatchingPeriod={50}
                 onScroll={handleScroll}
                 scrollEventThrottle={16}
                 renderItem={({ item }) => (

commit 2f4932821b2897904a95eee6f5fde34ad41a70e5

fix: remove stray numColumns from models FlatList (regression in dcf99e1)

src/screens/ExploreScreen.tsx
@@ -268,7 +268,6 @@ const ExploreScreen = () => {
               <FlatList
                 data={displayedModels}
                 keyExtractor={(item) => item.id.toString()}
-                numColumns={numColumns}
                 style={styles.flexFill}
                 contentContainerStyle={[
                   styles.listContent,